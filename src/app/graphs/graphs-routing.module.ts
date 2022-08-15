a6e46bf843e30e2c5c7a3d449def11c7a5b62038
eduardo_dorado <eduardo.dorado@sanmina.com>
2022-08-15 17:54:53 -0500

    update - generage component routers and show graph pages

--- src/app/graphs/graphs-routing.module.ts
+++ src/app/graphs/graphs-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarComponent } from './pages/bar/bar.component';
+import { BarDoubleComponent } from './pages/bar-double/bar-double.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'bar',        component: BarComponent },
+      { path: 'bar-double', component: BarDoubleComponent },
+      { path: 'dona',       component: DonaComponent },
+      { path: 'dona-http',  component: DonaHttpComponent },
+      { path: '**',         redirectTo: 'bar' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
