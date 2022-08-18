b4066a937935af2011e9396a9d0342ba0ffbc934
eduardo_dorado <eduardo.dorado@sanmina.com>
2022-08-18 16:12:56 -0500

    update - custom bar-chart finished ( data, labels )

--- src/app/graphs/components/bar-chart/bar-chart.component.ts
+++ src/app/graphs/components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -6,22 +6,12 @@ import { BaseChartDirective } from 'ng2-charts';
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
+
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-      }
-    }
-  };
-  public barChartType: ChartType = 'bar';
-  // public barChartType: ChartType = 'line';
-
-  public barChartData: ChartData<'bar'> = {
+  @Input() horizontal:boolean = false;
+  @Input() public barChartData: ChartData<'bar'> = {
     labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027'],
     datasets: [
       { data: [ 65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#8979F7', hoverBackgroundColor: 'red' },
@@ -29,6 +19,25 @@ export class BarChartComponent {
       { data: [ 28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor: '#F760BB', hoverBackgroundColor: 'red' },
     ],
   };
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      }
+    },
+    indexAxis: 'x',
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  // public barChartType: ChartType = 'line';
+
 
   // events
   public chartClicked({
@@ -50,6 +59,21 @@ export class BarChartComponent {
   }): void {
     console.log(event, active);
   }
+
+
+  ngOnInit(): void {
+    console.log(this.barChartData);
+
+    //Si el grafico es horizontal
+    if(this.horizontal) {
+      //Para invertir las barras y colocarlas horizontalmente necesitamos cambiar el "indexAxis"
+      this.barChartOptions!.indexAxis = 'y';
+      //Para que se nos muestren todos los datos horizontalmente también debemos retirar el
+      //"min" que tenemos en el barChartOptions.
+      this.barChartOptions!.scales!["y"]!.min = 0;
+    }
+  }
+
 
   public randomize(): void {
     // Only Change 3 values
@@ -73,12 +97,6 @@ export class BarChartComponent {
       Math.round( Math.random() * 100 ),
       Math.round( Math.random() * 100 ),
     ];
-
-    // this.barChartData.datasets[0].data = [
-    //   Math.round(Math.random() * 100), 59, 80,
-    //   Math.round(Math.random() * 100), 56,
-    //   Math.round(Math.random() * 100), 40,
-    // ];
 
     this.chart?.update();
   }
--- src/app/graphs/pages/bar-double/bar-double.component.ts
+++ src/app/graphs/pages/bar-double/bar-double.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-bar-double',
   templateUrl: './bar-double.component.html',
 })
-export class BarDoubleComponent implements OnInit {
+export class BarDoubleComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  myBarChartData: ChartData<'bar'> = {
+    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027'],
+    datasets: [
+      { data: [ 50, 50, 50, 50, 50, 50, 100], label: 'Series A', backgroundColor: '#8979F7', hoverBackgroundColor: 'red' },
+      { data: [ 50, 50, 50, 50, 50, 50, 100], label: 'Series B', backgroundColor: '#D7F76D', hoverBackgroundColor: 'red' },
+      { data: [ 50, 50, 50, 50, 50, 50, 100], label: 'Series C', backgroundColor: '#F760BB', hoverBackgroundColor: 'red' },
+    ],
+  };
 
 }
