b87314ab66fbac40936754a705627318d4adf38a
eduardo_dorado <eduardo.dorado@sanmina.com>
2022-08-18 19:40:18 -0500

    update - show request info from social media users

--- src/app/graphs/pages/dona-http/dona-http.component.ts
+++ src/app/graphs/pages/dona-http/dona-http.component.ts
@@ -1,23 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 import { ChartsService } from '../../services/charts.service';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
 })
-export class DonaHttpComponent implements OnInit {
+export class DonaHttpComponent implements OnInit{
+
+ // Doughnut
+ doughnutChartLabels: string[] = [
+  // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+];
+ doughnutChartData: ChartData<'doughnut'> = {
+   labels: this.doughnutChartLabels,
+   datasets: [
+     {
+      //  data: [ 350, 450, 100 ],
+       data: [  ],
+       backgroundColor: [
+         '#0075ED',
+         '#00BAF7',
+         '#00E0DB'
+       ]
+     }
+   ]
+ };
+ doughnutChartType: ChartType = 'doughnut';
+
+ // events
+ chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+   console.log(event, active);
+ }
+
+ chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+   console.log(event, active);
+ }
 
   constructor(
     private _chartService: ChartsService
   ) { }
 
   ngOnInit(): void {
-
-  }
-
-  getData() {
     this._chartService.getUsersSocialMedia()
-      .subscribe( data => console.log(data) );
+      .subscribe( resp => {
+        const labels = Object.keys( resp );
+        const values = Object.values( resp );
+
+        this.doughnutChartData = {
+          labels: labels,
+          datasets: [
+            {
+              data: values,
+              backgroundColor: [
+                '#0075ED',
+                '#00BAF7',
+                '#00E0DB'
+              ]
+            }
+          ]
+        }
+      });
   }
 
 }
